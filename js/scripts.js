1cb3aed80e690ae1422fb469fe374fb2c98cb4a6
Piotrek <mydifolt@gmail.com>
2017-05-16 15:05:14 +0200

    2nd trial

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// project 8-5. script85.js
+// calculating the area T of a triangle
+// b is the length of the base of the triangle
+// h is the height or altitude of the triangle
+
+var a = prompt('Wpisz prosze dlugosc podstawy trojkata w cm');
+
+var h = prompt('Wpisz prosze wysokosc trokata w cm');
+
+var triangleArea = a*h*1/2;
+
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
